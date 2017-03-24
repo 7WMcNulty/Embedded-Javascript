b0fa7b9abe504dde2ff99928df7e0259a7cb3a93
7WMcNulty <7WMcNulty@stpatrick.org>
2017-03-24 13:56:23 -0500

    Finally got tablet size

--- resize.js
+++ resize.js
@@ -1,4 +1,4 @@
-//Declare tabletSize() here
+/*//Declare tabletSize() here
 function tabletSize(){
     const doc = document;
     let sections = doc.querySelectorAll("section");
@@ -24,13 +24,35 @@ function phoneSize(){
         sections[i].style.height = height;
         sections[i].style.width = width;
     }
-}
-/*
+}*/
+
 function tabletSize(){
   const doc = document;
   let sections = doc.querySelectorAll("section");
   for(var i=0; i<sections.length; i++){
-    sections[i].style.width *= 0.75;
-    sections[i].style.height *= 0.75;
+    var height = window.getComputedStyle(sections[i],null).getPropertyValue("height");
+    let pIndex = height.indexOf("p");
+    let newHeight = "";
+    for(var j=0; j<pIndex; j++){
+      let temp = height.charAt(j);
+      newHeight = newHeight + temp;
+    }
+    newHeight*=0.75;
+    newHeight = newHeight+"px"
+    newHeight.toString();
+    sections[i].style.height = newHeight;
   }
-}*/
+  for(var i=0; i<sections.length; i++){
+    var width = window.getComputedStyle(sections[i],null).getPropertyValue("width");
+    let qIndex = width.indexOf("p");
+    let newWidth = "";
+    for(var j=0; j<qIndex; j++){
+      let temp = width.charAt(j);
+      newWidth = newWidth + temp;
+    }
+    newWidth*=0.75;
+    newWidth = newWidth+"px"
+    newWidth.toString();
+    sections[i].style.width = newWidth;
+  }
+}
